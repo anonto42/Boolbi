68b1926e64324d4022c8dcd0ff69a7c090fb490a
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-05 15:15:04 +0600

    creating user interfacs

--- src/Interfaces/User.interface.ts
+++ src/Interfaces/User.interface.ts
@@ -1,9 +1,21 @@
 import { Document } from "mongoose";
+import { ACCOUNT_STATUS, ACCOUNT_VERIFICATION_STATUS, SELECTED_LANGUAGE } from "../enums/user.enums";
 
 export interface IUser extends Document {
-    name: string;
-    email?: string;
-    role?: "user" | "admin";
+    fullName: string;
+    email: string;
+    role: string;
+    password: string;
+    category: string;
+    subCatagory: string;
+    city: string;
+    postalCode: string;
+    address: string;
+    samplePictures: string[];
+    accountVerificationsPictures: string[];
+    accountActivityStatus: ACCOUNT_STATUS;
+    language: SELECTED_LANGUAGE;
+    profile_verification: ACCOUNT_VERIFICATION_STATUS;
     createdAt: Date;
     updatedAt: Date;
 }
