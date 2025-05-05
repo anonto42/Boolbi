8c01f6ab630241631097b03664f34246af2860ca
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-05 14:24:21 +0600

    global error handler

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,71 @@
+import { ErrorRequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
+import ApiError from '../errors/ApiError';
+import { IErrorMessage } from '../types/errors.types';
+import { errorLogger } from '../shared/logger';
+
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  process.env.NODE_ENV === 'development'
+    ? console.log('🚨 globalErrorHandler ~~ ', error)
+    : errorLogger.error('🚨 globalErrorHandler ~~ ', error);
+
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages: IErrorMessage[] = [];
+
+  if (error.name === 'ZodError') {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error.name === 'TokenExpiredError') {
+    statusCode = StatusCodes.UNAUTHORIZED;
+    message = 'Session Expired';
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message:
+              'Your session has expired. Please log in again to continue.',
+          },
+        ]
+      : [];
+  } else if (error instanceof ApiError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: process.env.NODE_ENV !== 'production' ? error?.stack : undefined,
+  });
+};
+
+export default globalErrorHandler;
