ba9a01df2d1d62e2880277feba3ad62df740eea5
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-06 11:49:10 +0600

    offer

--- src/model/offer.model.ts
+++ src/model/offer.model.ts
@@ -1,13 +1,62 @@
 import { model, models, Schema } from "mongoose";
+import { IOffer } from "../Interfaces/offer.interface";
 
-const offerSchema = new Schema({
-    jobId: { type: Schema.Types.ObjectId, ref: 'JobPost' },
-    providerId: { type: Schema.Types.ObjectId, ref: 'User' },
-    files: [String],
-    price: Number,
-    status: { type: String, enum: ['pending', 'accepted', 'declined', 'withdrawn'], default: 'pending' },
-    message: String
-  });
+const offerSchema = new Schema<IOffer>({
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  serviceProvider: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  companyName: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 8
+  },
+  projectName:{
+    type: String,
+    required: true,
+    trim: true
+  },
+  catagory:{
+    type: String,
+    required: true,
+    trim: true
+  },
+  subCatagory:{
+    type: String,
+    required: true,
+    trim: true
+  },
+  budget:{
+    type: Number,
+    required: true,
+    trim: true
+  },
+  jobLocation:{
+    type: String,
+    required: true,
+    trim: true
+  },
+  deadline:{
+    type: String,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true,
+    trim: true,
+    min: 8
+  },
+  companyImages:[{
+    type: String,
+    required: true,
+    trim: true
+  }]
+});
 
 const Offer = models.User || model('offer', offerSchema);
 export default Offer;
