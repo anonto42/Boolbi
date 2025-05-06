950be71d4054f7144691a9dfd1cc7952b8cafb93
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-06 12:11:04 +0600

    post interface created

---
+++ src/Interfaces/post.interface.ts
@@ -0,0 +1,13 @@
+import { Document, Types } from "mongoose"
+
+export interface IPost extends Document {
+    title: string;
+    postType: string;
+    catagory: string;
+    companyName: string;
+    location: string;
+    deadline: string; 
+    jobDescription: string;
+    showcaseImages: string[];
+    creatorID: Types.ObjectId;
+}
