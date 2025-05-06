d9e920416549042efb20745a567b524256df1c0e
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-06 14:19:36 +0600

    message schema

--- src/model/message.model.ts
+++ src/model/message.model.ts
@@ -1,9 +1,24 @@
-const messageSchema = new mongoose.Schema({
-    orderId: mongoose.Schema.Types.ObjectId,
-    from: mongoose.Schema.Types.ObjectId,
-    to: mongoose.Schema.Types.ObjectId,
-    type: { type: String, enum: ['text', 'image'] },
-    content: String,
-    timestamp: { type: Date, default: Date.now }
-  });
-  
+import { model, models, Schema } from "mongoose";
+import { IMessage } from "../Interfaces/message.interface";
+
+const messageSchema = new Schema<IMessage>({
+  sender:{
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  receiver:{
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  content:{
+    type: String,
+    trim: true,
+    max: 1400
+  },
+  image:[{
+    type: String
+  }]
+});
+
+const Message = models.Order || model('message', messageSchema);
+export default Message;
