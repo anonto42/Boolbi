6aa2f460025f6dfbd8ce4ff3269ce0eee319618c
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-07 07:23:51 +0600

    added the auth endpoient

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { UserRouter } from '../app/router/user.route';
+import { AuthRouter } from '../app/router/auth.route';
 const router = express.Router();
 
 const apiRoutes = [
   {
     path: '/user',
     route: UserRouter,
-  }
+  },
+  {
+    path: '/auth',
+    route: AuthRouter,
+  },
 ];
 
 apiRoutes.forEach(route => router.use(route.path, route.route));
