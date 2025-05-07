355eda167f8c94ffcf056082fe750f6cb14f0ef4
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-07 10:05:18 +0600

    added the config input

--- src/helpers/jwtHelper.ts
+++ src/helpers/jwtHelper.ts
@@ -1,11 +1,35 @@
-import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import ApiError from '../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
+import config from '../config';
 
-const createToken = (payload: object, secret: Secret, expireTime: string) => {
-  return jwt.sign(payload, secret, { expiresIn: expireTime });
+export interface PayloadType{
+  userID: string,
+  role: string,
+  language: string,
+}
+
+const createToken = (payload: PayloadType): string => {
+  const secret = config.jwt_secret;
+  const expiresIn = config.jwt_expire;
+
+  if (!secret || !expiresIn) {
+    throw new ApiError(
+      StatusCodes.FAILED_DEPENDENCY,
+      'JWT_SECRET or JWT_EXPIRE_IN is not defined in environment variables.'
+    );
+  }
+
+  const options: any = {
+    expiresIn: expiresIn,
+  };
+
+  return jwt.sign(payload, secret as jwt.Secret, options);
 };
 
-const verifyToken = (token: string, secret: Secret) => {
-  return jwt.verify(token, secret) as JwtPayload;
+
+const verifyToken = (token: string) => {
+  return jwt.verify(token, config.jwt_expire) as JwtPayload;
 };
 
 export const jwtHelper = { createToken, verifyToken };
