231b2e0bd55f920178509fc344c0b099abadffe8
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-07 09:01:04 +0600

    signup validation

--- src/validation/IO.validation.ts
+++ src/validation/IO.validation.ts
@@ -2,12 +2,16 @@ import { z } from 'zod';
 
 const singnUpZodSchema = z.object({
   body: z.object({
+    role: z.string({ required_error: "You must give your account type"}),
+    fullName: z.string({ required_error: 'Full Name is required' }),
+    phone: z.string().optional(),
+    password: z.string({ required_error: 'Password is required' }),
+    confirmPassword: z.string({ required_error: 'Confirm password is required' }),
     email: z.string({ required_error: 'Email is required' }),
-    oneTimeCode: z.number({ required_error: 'One time code is required' }),
   }),
 });
 
 
-export const AuthValidation = {
+export const Validation = {
   singnUpZodSchema
 };
