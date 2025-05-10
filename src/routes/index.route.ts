c01ef62086e8ee326aeb288f86085edbd44a4cd4
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-10 08:28:06 +0600

    provider added

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRouter } from '../app/router/user.route';
 import { AuthRouter } from '../app/router/auth.route';
+import { ProviderRoter } from '../app/router/provider.route';
 const router = express.Router();
 
 const apiRoutes = [
@@ -12,6 +13,10 @@ const apiRoutes = [
     path: '/auth',
     route: AuthRouter,
   },
+  {
+    path: '/provider',
+    route: ProviderRoter
+  }
 ];
 
 apiRoutes.forEach(route => router.use(route.path, route.route));
