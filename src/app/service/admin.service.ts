be29eeb00bd6844f864c1183c81a985f0b56f587
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-13 09:43:06 +0600

    admin data over view

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -4,6 +4,7 @@ import ApiError from "../../errors/ApiError";
 import { StatusCodes } from "http-status-codes";
 import Offer from "../../model/offer.model";
 import Post from "../../model/post.model";
+import Payment from "../../model/payment.model";
 
 // Need more oparation for the best responce
 const overview = async (
@@ -18,14 +19,48 @@ const overview = async (
     const totalUser = await User.find({});
     const totalJobRequest = await Offer.find({});
     const totalJobPost = await Post.find({});
-    const totalRevenue = user.accountBalance;
+    
+    const result = await Payment.aggregate([
+        {
+            $match: {
+            status: "SUCCESS",
+            },
+        },
+        {
+            $group: {
+            _id: {
+                year: { $year: "$createdAt" },
+                month: { $month: "$createdAt" },
+            },
+            monthlyCommission: { $sum: "$commission" },
+            },
+        },
+        {
+            $sort: { "_id.year": 1, "_id.month": 1 }
+        }
+        ]);
+
+        const totalRevenue = result.reduce((sum, item) => sum + item.monthlyCommission, 0);
+
+        const monthlyRevenue = result.map(item => {
+            const monthName = new Date(item._id.year, item._id.month - 1).toLocaleString("default", { month: "long" });
+            return {
+                month: monthName,
+                year: item._id.year,
+                revenue: item.monthlyCommission,
+            };
+        });
+    
 
     return {
         totalJobPost,
         totalJobRequest,
         totalUser,
-        totalRevenue
-    } // total Users, Revenue, Job Request, Job post
+        totalRevenue:{
+            totalRevenue,
+            monthlyRevenue
+        }
+    };
 }
 
 export const AdminService = {
