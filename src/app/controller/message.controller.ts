ec1ae634e958fa2f19f1c1077f9b721a2da3df30
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-20 12:09:10 +0600

    get all the chat room that I'm in the room and a single chat room

--- src/app/controller/message.controller.ts
+++ src/app/controller/message.controller.ts
@@ -1,30 +1,14 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
-import { MessageService } from "../service/message.service";
+import { messageService } from "../service/message.service";
 import sendResponse from "../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
-import { getSingleFilePath } from "../../shared/getFilePath";
-
-const sendMessage = catchAsync(
-    async( req: Request, res: Response ) => {
-        const payload = (req as any).user;
-        const {...data} = req.body;
-        const image = getSingleFilePath(req.files,"image");
-        const result = await MessageService.sendMessageSend(payload,data,image as string);
-
-        sendResponse(res, {
-            success: true,
-            statusCode: StatusCodes.OK,
-            message: "message send successfull",
-            data: result
-        })
-    }
-)
+import { chatService } from "../service/chat.service";
 
 const chatRooms = catchAsync(
     async( req: Request, res: Response ) => {
         const payload = (req as any).user;
-        const result = await MessageService.chatRooms(payload);
+        const result = await chatService.getMyChatList(payload.userID);
 
         sendResponse(res, {
             success: true,
@@ -37,38 +21,19 @@ const chatRooms = catchAsync(
 
 const singleChatRoom = catchAsync(
     async( req: Request, res: Response ) => {
-        const payload = (req as any).user;
-        const roomID = req.query.roomID;
-        const postID = req.query.postID;
-        const result = await MessageService.getSingleChatRoom(payload,roomID as string,postID as string);
+        const roomID = req.query.chatID;
+        const result = await messageService.getMessages(roomID as string,{limit: 10,page: 6});
 
         sendResponse(res, {
             success: true,
             statusCode: StatusCodes.OK,
-            message: `Successfull get the ${ result.chatName } chat room`,
-            data: result
-        })
-    }
-)
-
-const createChatRooms = catchAsync(
-    async( req: Request, res: Response ) => {
-        const payload = (req as any).user;
-        const {...data} = req.body
-        const result = await MessageService.createChatRoom(payload,data);
-
-        sendResponse(res, {
-            success: true,
-            statusCode: StatusCodes.OK,
-            message: "Successfull created the chat room",
+            message: `Successfull get the chats`,
             data: result
         })
     }
 )
 
 export const MessageController = {
-    sendMessage,
     chatRooms,
-    singleChatRoom,
-    createChatRooms
+    singleChatRoom
 }
