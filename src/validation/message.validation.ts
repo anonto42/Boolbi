f4d4067075262cec98aa9880d0f34d7c9d9081d2
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-20 12:10:32 +0600

    configard the validation for the messages

--- src/validation/message.validation.ts
+++ src/validation/message.validation.ts
@@ -1,30 +1,11 @@
 import { z } from "zod";
 
-
 const getChatRroom = z.object({
-    quary: z.object({
-        roomID: z.string({required_error:"You must give the room id"}),
-        postID: z.string({required_error:"You must give the room id"}),
-    })
-})
-
-const sendMessageSchema = z.object({
-    body: z.object({
-        message: z.string().optional(),
-        chatRoom: z.string({required_error:"You must give the chat room locaton or id to send the message"})
-    })
-})
-
-const careateChat = z.object({
-    body: z.object({
-        chatWith: z.string({required_error:"you must give the recever id"}), 
-        chatName: z.string({required_error:"You must give the name of the chat id"}), 
-        image: z.string({required_error:"you must give the chat room logo image url"})
+    query: z.object({
+        chatID: z.string().optional()
     })
 })
 
 export const MessageValidation = {
-    getChatRroom,
-    sendMessageSchema,
-    careateChat
+    getChatRroom
 }
