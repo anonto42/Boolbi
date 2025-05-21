bfb0f782ff28437016ad212e170e4ea67526ae86
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-21 14:09:15 +0600

    firebase helper

--- src/helpers/firebaseHelper.ts
+++ src/helpers/firebaseHelper.ts
@@ -1,10 +1,16 @@
-import admin from "firebase-admin";
-import serviceAccount from "../../service_account_key.json";
+import { initializeApp, cert  } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
+import  "../../service_account_key.json";
 import ApiError from "../errors/ApiError";
 import { StatusCodes } from "http-status-codes";
+import config from "../config";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any)
+const base64key = config.firebase_service_account_key!;
+const firbaseServiceAccountKey = Buffer.from(base64key,"base64").toString("utf-8");
+const servireAccountKey = JSON.parse(firbaseServiceAccountKey);
+
+initializeApp({
+  credential: cert(servireAccountKey)
 });
 
 interface message {
@@ -15,10 +21,22 @@ interface message {
   token: string
 }
 
-export const messageSend = async (props: message) => {
+export const messageSend = async (msg: message) => {
   try {
-    const response = await admin.messaging().send(props)
-    console.log(response)
+    
+    await getMessaging()
+          .send(msg)
+          .then( response => {
+            console.log(response)
+            return {
+              message: "Successfully send the message",
+              status: true
+            }
+          })
+          .catch( err => {
+            console.log(err)
+          });
+    
   } catch (error) {
     console.log(error)
     throw new ApiError(StatusCodes.EXPECTATION_FAILED,"Error hapending on the push message")
