5318b47e88977181bb81f6e45f1893f7c4206e73
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-22 08:05:54 +0600

    chat related oparation

--- src/validation/message.validation.ts
+++ src/validation/message.validation.ts
@@ -2,10 +2,27 @@ import { z } from "zod";
 
 const getChatRroom = z.object({
     query: z.object({
+        chatID: z.string({required_error: "You must give the chat id to get the chat id"})
+    })
+})
+
+const createChatRoom = z.object({
+    body: z.object({
+        image: z.string(),
+        chatName: z.string({ required_error: "you must give a chat name"},),
+        receiver: z.string({required_error: "You must give the second person of the user that you want to chat"})
+    })
+})
+
+const deleteRoom = z.object({
+    query: z.object({
         chatID: z.string().optional()
     })
 })
 
+
 export const MessageValidation = {
-    getChatRroom
+    getChatRroom,
+    createChatRoom,
+    deleteRoom
 }
