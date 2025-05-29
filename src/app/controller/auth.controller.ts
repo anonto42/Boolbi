08e2feccf2b5f59d17ee6015c69c495bcea3a33f
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-29 12:49:55 +0600

    auth removed the responce data from the headers

--- src/app/controller/auth.controller.ts
+++ src/app/controller/auth.controller.ts
@@ -3,29 +3,27 @@ import catchAsync from "../../shared/catchAsync"
 import sendResponse from "../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
 import { AuthServices } from "../service/auth.service";
-import { USER_ROLES } from "../../enums/user.enums";
-import config from "../../config";
 
 const SignIn = catchAsync(
     async( req: Request, res: Response ) => {
         const {...data} = req.body;
         const result = await AuthServices.signIn(data);
 
-        if (result.user.role === USER_ROLES.ADMIN || result.user.role === USER_ROLES.SUPER_ADMIN) {
-            const expirDate = Number(config.jwt_expire.split("")[0])
-            return res
-                    .cookie('authorization', result.token, {
-                        httpOnly: true,
-                        secure: config.node_env === 'production',
-                        sameSite: 'strict',
-                        maxAge: expirDate
-                    })
-                    .json({
-                        sucess: true,
-                        statusCode: StatusCodes.OK,
-                        message: "Welcome to deshboard",
-                    })
-        }
+        // if (result.user.role === USER_ROLES.ADMIN || result.user.role === USER_ROLES.SUPER_ADMIN) {
+        //     const expirDate = Number(config.jwt_expire.split("")[0])
+        //     return res
+        //             .cookie('authorization', result.token, {
+        //                 httpOnly: true,
+        //                 secure: config.node_env === 'production',
+        //                 sameSite: 'strict',
+        //                 maxAge: expirDate
+        //             })
+        //             .json({
+        //                 sucess: true,
+        //                 statusCode: StatusCodes.OK,
+        //                 message: "Welcome to deshboard",
+        //             })
+        // }
         sendResponse(res, {
             success: true,
             statusCode: StatusCodes.OK,
